c6d861e83348c445205d45161b5f5ea98b657832
via2121 <virajadhav2121.gmail.com>
2022-03-16 16:52:17 +0530

    Substraction operation completed

--- src/com/harman/calculator/Calculator.java
+++ src/com/harman/calculator/Calculator.java
@@ -2,8 +2,11 @@ package com.harman.calculator;
 
 public class Calculator {
     public static void main(String[] args){
+        Subtraction ob=new Subtraction();
         int a=5,b=6,c;
         c=a+b;
         System.out.println(c);
+        int result=ob.sub(a,b);
+        System.out.println(result);
     }
 }
---
+++ src/com/harman/calculator/Subtraction.java
@@ -0,0 +1,7 @@
+package com.harman.calculator;
+
+public class Subtraction {
+    public int sub(int x,int y){
+        return x-y;
+    }
+}
